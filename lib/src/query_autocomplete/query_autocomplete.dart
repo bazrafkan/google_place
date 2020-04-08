5693406624f3b298df10d2ea58deffce2a0c09e8
bazrafkan <bazrafkan.mb@gmail.com>
2020-04-08 17:38:33 +0200

    chore: update description for query autocomplete

--- lib/src/query_autocomplete/query_autocomplete.dart
+++ lib/src/query_autocomplete/query_autocomplete.dart
@@ -10,12 +10,28 @@ class QueryAutocomplete {
 
   QueryAutocomplete(this.apiKEY);
 
-  /// Create a deprecation annotation which specifies the migration path and
-  /// expiration of the annotated feature.
+  /// The Query Autocomplete service can be used to provide a query prediction for text-based
+  /// geographic searches, by returning suggested queries as you type.
   ///
-  /// The [input] argument should be readable by programmers, and should state
-  /// an alternative feature (if available) as well as when an annotated feature
-  /// is expected to be removed.
+  /// [input] Required parameters - The text string on which to search. The Places service will return
+  /// candidate matches based on this string and order results based on their perceived relevance.
+  ///
+  /// [offset] Optional parameters - The character position in the input term at which the service
+  /// uses text for predictions. For example, if the input is 'Googl' and the completion point is 3,
+  /// the service will match on 'Goo'. The offset should generally be set to the position of the text
+  /// caret. If no offset is supplied, the service will use the entire term.
+  ///
+  /// [location] Optional parameters - The point around which you wish to retrieve place information.
+  /// Must be specified as latitude,longitude.
+  ///
+  /// [radius] Optional parameters - The distance (in meters) within which to return place results.
+  /// Note that setting a radius biases results to the indicated area, but may not fully restrict
+  /// results to the specified area.
+  /// [language] Optional parameters - The language code, indicating in which language the results
+  /// should be returned, if possible. Searches are also biased to the selected language; results in
+  /// the selected language may be given a higher ranking. See the supported list of domain languages.
+  /// If language is not supplied, the Places service will attempt to use the native language of the
+  /// domain from which the request is sent.
   Future<AutocompleteResponse> get(
     String input, {
     int offset,
